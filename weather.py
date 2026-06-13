d9128b93d0ac872afcd371611341c04555e3d872
rajthummar557 <thummarraj60@gmail.com>
2026-06-13 19:23:48 +0530

    Delete weather.py

--- weather.py
+++
@@ -1,5 +0,0 @@
-import requests
-url="https://api.openweathermap.org/data/2.5/weather?q=ahmedabad&appid=d7549fa26fcb7426e6cd9d0d48cc4370"
-response=requests.get(url)
-data=response.json()
-print(data)
